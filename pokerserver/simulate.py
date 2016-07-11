f23da95a465c689487bd887dad326b76a065b68d
Martin Altmayer <martin.altmayer@tngtech.com>
2016-07-11 22:54:32 +0200

    #18 Created simple client and simulate script.

---
+++ pokerserver/simulate.py
@@ -0,0 +1,26 @@
+import threading
+import random
+import argparse
+
+from pokerserver.simpleclient import Client
+
+NUMBER_OF_PLAYERS = 2
+
+
+def get_random_name():
+    return 'Player{}'.format(random.randint(1, 99))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Start several clients in parallel.')
+    parser.add_argument('-n', '--number', type=int, help='Number of clients', default=2)
+    args = parser.parse_args()
+
+    for _ in range(args.number):
+        client = Client('localhost', 5555, get_random_name(), None)
+        client_thread = threading.Thread(target=client.play)
+        client_thread.start()
+
+
+if __name__ == "__main__":
+    main()
